96a981da74d898c1837a9ef18f13d6c69141ea03
Caio Arthur <cast@icomp.ufam.edu.br>
2020-09-20 15:12:38 -0400

    addind notes

--- notes-app/notes/storage.js
+++ notes-app/notes/storage.js
@@ -1,14 +1,27 @@
 const fs = require('fs')
 
-const save = args => fs.writeFileSync(`./notes/${args.title}.json`, args.json) 
+const save = 
+    args => 
+        fs.writeFileSync(`./notes/${args.title}.json`, args.json) 
+
+const saveNote = args => {
+    const dataJSON = JSON.stringify(args)
+    fs.writeFileSync(`./notes/notes.json`, dataJSON) 
+}
 
 const read = args => {
-    const dataBuffer = fs.readFileSync(`./notes/${args.title}.json`)
-    const dataJSON = dataBuffer.toString()
-    return JSON.parse(dataJSON)
+
+    try {
+        const dataBuffer = fs.readFileSync(`./notes/${args}.json`)
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
 }
 
 module.exports = {
     save,
-    read
+    read,
+    saveNote
 }
